7bdb05e7b5fbfce06f1c9d072dc1db7972bc484c
Deeplaxmi Lad <deeplaxmilad518@gmail.com>
2026-02-15 22:46:19 +0530

    Fixed express wildcard route and PORT

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,41 @@
-const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://deeplaxmilad518_db_user:qedeo1oQL1hZV0pe@cluster0.oyab0ma.mongodb.net/?appName=Cluster0")
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
+// API Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
 
-const path = require("path");
+// ================== SERVE FRONTEND ==================
 
-// Serve React build folder
+// IMPORTANT: your build is inside backend/build
 app.use(express.static(path.join(__dirname, "build")));
 
-// Catch-all route for React
-app.get("*", (req, res) => {
+// Use /* instead of *
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
